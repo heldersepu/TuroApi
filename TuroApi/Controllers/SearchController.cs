4eeaf0f5c0066c4f215a8021676fd71dbec20bd3
HelderSepu <HelderSepu@gmail.com>
2017-06-06 15:42:16 -0400

    TuroApi

---
+++ TuroApi/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Http;
+using TuroApi.Models;
+
+namespace TuroApi.Controllers
+{
+    public class SearchController : BaseController
+    {
+        // GET: api/Search
+        public IHttpActionResult GetByLocation(GeoPoint location, int items = 200, string make = null, string model = null)
+        {
+            var cars = TuroSearch(location, items, make, model);
+            return Ok(cars.OrderByDescending(c => c.tripsTaken));
+        }
+    }
+}
---
+++ TuroApi/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace TuroApi.Models
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public int tripsTaken;
+        public double dailyPrice;
+    }
+}
