929ec3f8b009055349a0d8856e6ab996ad2689b5
HelderSepu <heldersepu@gmail.com>
2017-06-07 21:52:24 -0400

    Add StatsController

---
+++ TuroApi/Controllers/StatsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Web.Http;
+using TuroApi.Models;
+
+namespace TuroApi.Controllers
+{
+    public class StatsController : BaseController
+    {
+        public IHttpActionResult Get(GeoPoint location, int items = 200)
+        {
+            var cars = TuroSearch(location, items);
+            return Ok(new CarStats {
+                total = cars.Count,
+                year = cars.GroupBy(x => x.year).ToDict(),
+                make = cars.GroupBy(x => x.make).ToDict(),
+                model = cars.GroupBy(x => x.model).ToDict(),
+            });
+        }
+    }
+}
---
+++ TuroApi/Models/CarStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TuroApi.Models
+{
+    public class CarStats
+    {
+        public int total;
+        public Dictionary<int, int> year;
+        public Dictionary<string, int> make;
+        public Dictionary<string, int> model;
+    }
+}
