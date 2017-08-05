093b01c6b89b5c79e873a11327570ef9c78e80a7
HelderSepu <heldersepu@gmail.com>
2017-08-05 11:24:11 -0400

    Add EchoController

---
+++ TuroApi/Controllers/EchoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using TuroApi.Models;
+
+namespace TuroApi.Controllers
+{
+    public class EchoController : ApiController
+    {
+        // GET: api/Echo
+        /// <summary>
+        /// Echo test for the location
+        /// </summary>
+        /// <param name="location">SoFL= 26.16,-80.20</param>
+        /// <returns>String with Latitude and Longitude</returns>
+        public string Get(GeoPoint location)
+        {
+            return $"Latitude={location.Latitude}, Longitude={location.Longitude}";
+        }
+    }
+}
